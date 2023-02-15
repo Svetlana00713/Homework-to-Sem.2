479f900cb5d97f08fa1a4acb2db364b3795328ca
Svetlana <start61@mail.ru>
2023-02-15 19:34:02 +0300

    Added tasks 10,13,15

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write("Введи число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string NText = Convert.ToString(N);
+if (NText.Length > 2)
+{
+  Console.WriteLine("Третья цифра: " + NText[2]);
+}
+else 
+{
+  Console.WriteLine("Третьей цифры нет");
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.Write("Введите цифру, обозначавющую день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+        Console.Write("Нет");
+        break;
+
+    case 2:
+        Console.Write("Нет");
+        break;
+
+    case 3:
+        Console.Write("Нет");
+        break;
+
+    case 4:
+        Console.Write("Нет");
+        break;
+
+    case 5:
+        Console.Write("Нет");
+        break;
+
+    case 6:
+        Console.Write("Да");
+        break;
+
+    case 7:
+        Console.Write("Да");
+        break;
+
+    default:
+        Console.Write("Неверное значение!");
+        break;
+}
+
